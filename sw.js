d1e917a3ab899995cf9f2b8c59afcb933f930244
edugrowth5-rgb <edugrowth5@gmail.com>
2026-03-02 17:49:22 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,25 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
+const CACHE_NAME = 'jokes-app-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/1023/1023656.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  // App ko load hone mein madad karta hai
+// Fetch resources
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
